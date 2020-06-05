33fcc6b3bd151e99ee8ba50338cf4bd9d4326b3f
Zied <zlouhichi@gmail.com>
2020-06-05 17:51:43 +0100

    testing

--- web/modules/custom/awsome/tests/src/Unit/HelloTest.php
+++ web/modules/custom/awsome/tests/src/Unit/HelloTest.php
@@ -4,6 +4,9 @@ namespace Drupal\Tests\awsome\Unit;
 use Drupal\Tests\UnitTestCase;
 
 class HelloTest{
+ 	/**
+     * @group hello
+     */	
 	function testHello(){
 		$hello = "hello";
 		$this->assertEqual("hello", $hello);
